94e90d4f0e4d33ec6b85ea43eada627ed7af9a1f
Ryan Collins <ryan@ryanmcollins.com>
2016-06-09 17:11:23 -0400

    fixed typo in database path

--- Sources/config.swift
+++ Sources/config.swift
@@ -18,12 +18,12 @@ import SQLite
 struct Config {
 
 	//Database Setup
-	let DB_PATH = "./db/sitedb/"
+	let DB_PATH = "./db/sitedb"
 
 	//Database Creation
 	func createDatabase() {
 		do {
-			let sqlite = try SQLite(AUTH_DB_PATH)
+			let sqlite = try SQLite(DB_PATH)
 			try sqlite.execute(statement: "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, fname TEXT NOT NULL, lname TEXT NOT NULL, email TEXT NOT NULL, pwd TEXT NOT NULL)")
 			try sqlite.execute(statement: "CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY NOT NULL, post_content TEXT NOT NULL, post_title TEXT NOT NULL)")
 		} catch {

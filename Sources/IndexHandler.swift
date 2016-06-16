274cb370aab5c6fcb01babd9d05e98b1e767ba85
Ryan Collins <ryan@ryanmcollins.com>
2016-06-16 16:21:59 -0400

    Added methods to retrieve homepage post by default and any other post with POST variable p=POST_ID

--- Sources/IndexHandler.swift
+++ Sources/IndexHandler.swift
@@ -17,29 +17,41 @@ import SQLite
 
 class IndexHandler: MustachePageHandler {
 
-    var content = "No Posts Were Found"
+    let DB_PATH: String = Config().getDatabasePath()
+
+    var contentID: String?
+
+    var content = "No Posts Were Found That Matched The Requested Post"
     var postTitle = "Welcome"
 
     func extendValuesForResponse(context contxt: MustacheEvaluationContext, collector: MustacheEvaluationOutputCollector) {
 
-        let DB_PATH: String = Config().getDatabasePath()
+        let request = contxt.webRequest
+        let params = request.params()
 
-        do {
-            let sqlite = try SQLite(DB_PATH)
-            defer {
-                sqlite.close()  // defer ensures we close our db connection at the end of this request
+        if params.count > 0 {
+            var parameters = [[String:Any]]()
+
+            for (name, value) in params {
+                parameters.append([
+                    name: value
+                    ])
             }
 
-            // query the db for a random post
-            try sqlite.forEachRow(statement: "SELECT post_content, post_title FROM posts WHERE id=(SELECT value FROM options WHERE option = 'front_page')") {
-                (statement: SQLiteStmt, i:Int) -> () in
-
-                    self.content = statement.columnText(position: 0)
-                    self.postTitle = statement.columnText(position: 1)
+            for dict in parameters {
+                for (key, value) in dict {
+                    if key == "p" {
+                        if let contentID = value as? String {
+                            loadPageContent(forPageID: contentID)
+                        }
+                    } else {
+                      loadPageContent(forPageID: nil, frontPage: true)
+                    }
                 }
+            }
 
-            } catch {
-              print("content retrieval failed")
+        } else {
+          loadPageContent(forPageID: nil, frontPage: true)
         }
 
         var values = MustacheEvaluationContext.MapType()
@@ -58,4 +70,32 @@ class IndexHandler: MustachePageHandler {
             response.requestCompleted()
         }
     }
+
+    func loadPageContent(forPageID: String?, frontPage: Bool = false) {
+      do {
+          let sqlite = try SQLite(DB_PATH)
+          defer {
+              sqlite.close()  // defer ensures we close our db connection at the end of this request
+          }
+
+          let sqlStatement: String
+          if !frontPage && forPageID != nil {
+              sqlStatement = "SELECT post_content, post_title FROM posts WHERE id= \(forPageID!)"
+          } else {
+              sqlStatement = "SELECT post_content, post_title FROM posts WHERE id=(SELECT value FROM options WHERE option = 'front_page')"
+          }
+
+          // query the db for a random post
+          try sqlite.forEachRow(statement: sqlStatement) {
+              (statement: SQLiteStmt, i:Int) -> () in
+
+                  self.content = statement.columnText(position: 0)
+                  self.postTitle = statement.columnText(position: 1)
+              }
+
+          } catch {
+
+        }
+    }
+
 }

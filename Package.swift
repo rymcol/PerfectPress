65ad50bd23c6f3bcefe8c70b45a699329b3a6ef3
Ryan Collins <ryan@ryanmcollins.com>
2016-08-10 15:32:45 -0400

    Removed SQLite from Package

--- Package.swift
+++ Package.swift
@@ -25,6 +25,5 @@ let package = Package(
 	dependencies: [
 	                  .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", versions: Version(0,0,0)..<Version(10,0,0)),
 					  .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", versions: Version(0,3,0)..<Version(10,0,0)),
-	                  .Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", versions: Version(0,3,0)..<Version(10,0,0))
     ]
 )

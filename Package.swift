3a2758a4b3b8a5fac0df93396dd840860976e9aa
Ryan Collins <ryan@ryanmcollins.com>
2016-08-19 13:23:41 -0400

    Updated to DEVELOPMENT-SNAPSHOT-08-18-a

--- Package.swift
+++ Package.swift
@@ -23,6 +23,6 @@ let package = Package(
 	name: "PerfectPress",
 	targets: [],
 	dependencies: [
-	                  .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 0, minor: 9),
+	                  .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 0, minor: 10),
     ]
 )
